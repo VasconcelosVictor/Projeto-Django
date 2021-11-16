30233691c94abff27d0633915be358dd4ba2c163
VasconcelosVictor <victorvasconcelos6x@hotmail.com>
2021-11-16 09:03:04 -0300

    Login e Logout

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,3 +1,4 @@
+from django import contrib
 from django.core import paginator
 from django.http.response import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
@@ -6,7 +7,9 @@ from django.core.paginator import Paginator
 from .forms import TaskForm
 from .models import Task
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def tasksList(request):
     search = request.GET.get('search')
     if search:
@@ -17,11 +20,11 @@ def tasksList(request):
         page = request.GET.get('page')
         tasks = paginator.get_page(page)
     return render(request, 'tasks/list.html', {'tasks': tasks} ) #Criando template django
-
+@login_required
 def taskView (request, id):
     task = get_object_or_404(Task, pk=id)
     return render(request, 'tasks/task.html', {'task': task})
-
+@login_required
 def newTask (request):
     if request.method == 'POST':
         form = TaskForm(request.POST)
@@ -34,7 +37,7 @@ def newTask (request):
     else:
         form = TaskForm()
         return render(request, 'tasks/addtask.html',{'form':form} )
-
+@login_required
 def editTask (request, id):
     task = get_object_or_404(Task, pk=id) 
     form = TaskForm(instance=task)
@@ -51,7 +54,7 @@ def editTask (request, id):
     else:
         return render(request, 'tasks/edittask.html', {'form': form, 'task': task })
 
-
+@login_required
 def deleteTask (request, id):
     task = get_object_or_404(Task, pk=id)
     task.delete()
--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'todo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,6 +125,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = '/'
+
+LOGOUT_REDIRECT_URL = '/accounts/login'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
